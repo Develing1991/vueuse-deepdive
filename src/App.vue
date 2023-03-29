39b4c4192aa06724f69798192279eaa8041ae050
Develing1991 <completed0728@gmail.com>
2023-03-30 06:14:14 +0900

    useMagicKeys, whenever

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,17 @@
 <script setup>
-import { ref } from 'vue';
-import { onKeyStroke } from '@vueuse/core';
-const position = ref({ x: 0, y: 0 });
-const controls = {
-  ArrowDown: () => (position.value.y += 5),
-  ArrowUp: () => (position.value.y -= 5),
-  ArrowRight: () => (position.value.x += 5),
-  ArrowLeft: () => (position.value.x -= 5),
-};
-onKeyStroke(Object.keys(controls), (e) => {
-  e.preventDefault();
-  controls[e.key]();
-});
-// onKeyStroke(['ArrowDown', 'ArrowUp', 'ArrowRight', 'ArrowLeft'], (e) => {
-//   e.preventDefault();
-//   //ArrowDown...
-//   if (e.key === 'ArrowDown') {
-//     position.value.y += 5;
-//   }
-//   if (e.key === 'ArrowUp') {
-//     position.value.y -= 5;
-//   }
-//   if (e.key === 'ArrowRight') {
-//     position.value.x += 5;
-//   }
-//   if (e.key === 'ArrowLeft') {
-//     position.value.x -= 5;
-//   }
-// });
-// onKeyStroke('ArrowDown', (e) => {
-//   e.preventDefault();
-//   position.value.y += 5;
-// });
-// onKeyStroke('ArrowUp', (e) => {
-//   e.preventDefault();
-//   position.value.y -= 5;
-// });
+import { ref, watch } from 'vue';
+import { useMagicKeys, whenever } from '@vueuse/core';
+
+const { a_b_c, shift_a, meta_a, ctrl_a, current } = useMagicKeys();
+whenever(a_b_c, (pressed) => console.log(pressed));
+// const { a, b, c } = useMagicKeys();
+// whenever(a, (pressed) => console.log(pressed));
+// whenever(b, (pressed) => console.log(pressed));
+// whenever(c, (pressed) => console.log(pressed));
 </script>
 
 <template>
-  <div class="ball"></div>
-  <input type="text" />
+  {{ current }}
 </template>
 
-<style>
-.ball {
-  width: 10px;
-  height: 10px;
-  background: red;
-  border-radius: 50%;
-  position: absolute;
-  top: v-bind(position.y + 'px');
-  left: v-bind(position.x + 'px');
-}
-</style>
+<style></style>
