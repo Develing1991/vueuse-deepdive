e3b2525c7c0767444d2d2ccdafbbf0a883c41176
Develing1991 <completed0728@gmail.com>
2023-03-30 04:10:19 +0900

    useClipboard

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <script setup>
-import { useTitle } from '@vueuse/core';
-const title = useTitle();
-title.value = 'vueuse title';
-console.log('page title', title.value);
+import { ref } from 'vue';
+import { useClipboard } from '@vueuse/core';
+
+const textToCopy = ref('Hello');
+// const { copy, copied, text, isSupported } = useClipboard({
+//   source: textToCopy,
+//   copiedDuring: 5000, // copied during time // default 2s
+// });
+const { copy, copied, text, isSupported } = useClipboard({
+  copiedDuring: 5000, // copied during time // default 2s
+});
 </script>
 
 <template>
-  <input type="text" v-model="title" />
+  <input type="text" v-model="textToCopy" />
+  <!-- <button @click="copy()">{{ copied ? 'Copied' : 'Copy' }}</button> -->
+  <button @click="copy(textToCopy)">{{ copied ? 'Copied' : 'Copy' }}</button>
+  <div>copied text : {{ text }}</div>
+  <div>isSupported : {{ isSupported }}</div>
 </template>
 
 <style scoped></style>
