6c689046af2002b02cb027dd9f744b9ebd693958
Develing1991 <completed0728@gmail.com>
2023-03-30 21:32:23 +0900

    3 files modified, 5 added

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useVModel } from '@vueuse/core';
-import CounterInput from './components/CounterInput.vue';
-const number = ref(0);
+import useAlertFavicon from './composables/useAlertFavicon';
+const vueFavicon =
+  'https://raw.githubusercontent.com/vueschool/useAlertFavicon-boilerplate/main/public/favicon.ico';
+const vueSchoolFavicon =
+  'https://raw.githubusercontent.com/vueschool/useAlertFavicon/main/public/vue-school-favicon.png';
+const { alert, favicon, cancel } = useAlertFavicon(vueFavicon, {
+  speed: 500,
+  emoji: '👻',
+});
 </script>
 
 <template>
-  <CounterInput v-model="number" />
-  <br />
-  {{ number }}
+  <button @click="alert">Alert</button>
+  <button @click="favicon = vueSchoolFavicon">Change</button>
+  <button @click="cancel">Cancel</button>
 </template>
