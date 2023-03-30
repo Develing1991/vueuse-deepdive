43baf8d2d602b23f708e4acfba1815f627a0cb76
Develing1991 <completed0728@gmail.com>
2023-03-30 20:40:33 +0900

    useVModel

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,12 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { useVirtualList } from '@vueuse/core';
-import ListItem from './components/ListItem.vue';
-const longList = ref(Array.from(Array(100_000).keys()));
-
-// containerProps : 전체 컨테이너 영역
-// wrapperProps : list의 바로 부모요소 (여기서는 ul)
-const { list, containerProps, wrapperProps } = useVirtualList(longList, {
-  itemHeight: 19, //실제 view에서 아이템 높이 (여기서는 li요소)
-  overscan: 10, // 그 높이만큼 10개 더 미리 출력
-});
+import { useVModel } from '@vueuse/core';
+import CounterInput from './components/CounterInput.vue';
+const number = ref(0);
 </script>
 
 <template>
-  <!-- <pre>{{ list }}</pre> -->
-  <div class="list-container" v-bind="containerProps">
-    <ul v-bind="wrapperProps">
-      <ListItem v-for="item in list" :id="item.data" :key="item.data" />
-    </ul>
-  </div>
+  <CounterInput v-model="number" />
+  <br />
+  {{ number }}
 </template>
-
-<style>
-html,
-body,
-#app,
-.list-container {
-  height: 100%;
-}
-</style>
---
+++ src/components/CounterInput.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { useVModel } from '@vueuse/core';
+const props = defineProps({
+  modelValue: Number,
+});
+const emit = defineEmits(['update:modelValue']);
+const data = useVModel(props, 'modelValue', emit);
+
+// data.value = 10; // emits update:modelValue
+</script>
+
+<template>
+  <button @click="data--">-</button>
+  <input type="number" v-model="data" />
+  <button @click="data++">+</button>
+</template>
+
+<style>
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type='number'] {
+  -moz-appearance: textfield;
+}
+</style>
