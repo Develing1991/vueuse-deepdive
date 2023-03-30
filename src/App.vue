5aa604fe1e05cec9152843011077076c36d37ae6
Develing1991 <completed0728@gmail.com>
2023-03-30 13:25:38 +0900

    useRafFn

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
 <script setup lang="ts">
-import { useIntervalFn } from '@vueuse/core';
-
-const { pause, resume, isActive } = useIntervalFn(
-  () => console.log('hello'),
-  100
-);
+import { ref } from 'vue';
+import { useIntervalFn, useRafFn } from '@vueuse/core';
+const activePosition = ref(0);
+const framesComplete = ref(0);
+// const { pause, resume, isActive } = useIntervalFn(() => {
+// 기본 1초에 60프레임 사용
+const { pause, resume, isActive } = useRafFn(() => {
+  framesComplete.value++;
+  // 60프레임을 6으로 나눴으니 1초에 10프레임씩 동작
+  if (framesComplete.value % 6) return; // 나머지가 0이면 동작 나머지는 truthy하니 리턴
+  console.log(framesComplete.value);
+  if (activePosition.value > -525) {
+    // 7 times frame
+    activePosition.value -= 75;
+  } else {
+    activePosition.value = 0;
+  }
+});
 </script>
 
 <template>
-  <button @click="isActive ? pause() : resume()">
-    {{ isActive ? 'pause' : 'resume' }}
-  </button>
+  <button @click="isActive ? pause() : resume()">Toggle</button>
+  <div
+    class="sprite"
+    :style="`background-position: ${activePosition}px 50%;`"
+  ></div>
 </template>
+
+<style>
+.sprite {
+  background: url(https://freesvg.org/img/1525205509.png) no-repeat;
+  width: 75px;
+  height: 150px;
+  /* background-position: -150px 50%; */
+}
+</style>
