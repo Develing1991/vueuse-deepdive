5af026945073866cde4c68e0b7e0298c974251d6
Develing1991 <completed0728@gmail.com>
2023-03-30 18:05:20 +0900

    vueuse/math

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue';
-import { computedAsync } from '@vueuse/core';
-const id = ref(1);
-const url = computed(
-  () => `https://jsonplaceholder.typicode.com/posts/${id.value}/`
-);
-const post = computedAsync(async () => {
-  // await post.get(id.value)
-  const res = await fetch(url.value);
-  return res.json();
-}, null);
+import { logicAnd, logicNot, logicOr } from '@vueuse/math';
+const hasBread = ref(true);
+const filledWithMeat = ref(true);
+const hasTwoSlicesOfBread = ref(true);
+const isAHotdog = ref(false);
+const isASandWich = computed(() => {
+  return logicAnd(
+    hasBread,
+    filledWithMeat,
+    hasTwoSlicesOfBread,
+    logicNot(isAHotdog)
+  );
+});
 </script>
 
 <template>
-  <input type="number" v-model="id" />
-  {{ post }}
+  {{ isASandWich }}
 </template>
