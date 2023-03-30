f35c23e08acd3b9bd282b868b57f5818d35bb968
Develing1991 <completed0728@gmail.com>
2023-03-30 12:56:23 +0900

    useInterval

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
-import { ref, watchEffect } from 'vue';
-import { useDisplayMedia } from '@vueuse/core';
-const video = ref();
-const { stream, enabled } = useDisplayMedia();
-// useUserMedia - camera
-// useMediaControls - video player
-watchEffect(() => {
-  if (video.value) {
-    video.value.srcObject = stream.value;
-  }
+import { useInterval } from '@vueuse/core';
+import { watch } from 'vue';
+
+const { counter, pause, resume, isActive } = useInterval(100, {
+  controls: true,
 });
+watch(counter, () => console.log('hello'));
 </script>
 
 <template>
-  <button @click="enabled = !enabled">Start Screen Share</button>
-  <video ref="video" muted autoplay controls width="800" />
+  {{ counter }}
+  <button @click="isActive ? pause() : resume()">
+    {{ isActive ? 'pause' : 'resume' }}
+  </button>
+  <button @click="counter = 0">Reset</button>
 </template>
