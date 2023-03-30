cbb02080ef3ec0e996211325660662b375016a95
Develing1991 <completed0728@gmail.com>
2023-03-30 19:19:34 +0900

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,32 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue';
-import { useWindowFocus } from '@vueuse/core';
-const focused = useWindowFocus();
+import { useIntersectionObserver } from '@vueuse/core';
+const h1 = ref(null);
+const h1IsVisible = ref(false);
+useIntersectionObserver(h1, ([{ isIntersecting }]) => {
+  h1IsVisible.value = isIntersecting;
+});
 </script>
 
-<template>{{ focused }}</template>
+<template>
+  <div style="height: 200px; overflow: scroll; border: 1px solid black">
+    <h1 :class="{ 'fade-in': h1IsVisible }" style="margin: 300px 0" ref="h1">
+      Hello
+    </h1>
+  </div>
+  {{ h1IsVisible }}
+</template>
+
+<style>
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+.fade-in {
+  animation: fadeIn ease 1s;
+}
+</style>
