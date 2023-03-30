93de43a92a0478e8aa77cc81fed8f8ce06275cd9
Develing1991 <completed0728@gmail.com>
2023-03-30 11:12:46 +0900

    onLongPress

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <script setup>
 import { ref, reactive, watch, onMounted } from 'vue';
 import { onLongPress } from '@vueuse/core';
+const longBtn = ref(null);
+onLongPress(
+  longBtn,
+  () => {
+    alert('Hello world');
+  },
+  {
+    delay: 3000,
+  }
+);
 </script>
 
 <template>
-  <button>Click and Hold</button>
+  <button ref="longBtn">Click and Hold</button>
 </template>
 
 <style></style>
