b44022ff1a90667077f79c1405e41818545f2f96
Develing1991 <completed0728@gmail.com>
2023-03-30 12:05:08 +0900

    useInfiniteScroll

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,55 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { useScroll } from '@vueuse/core';
+import { useInfiniteScroll } from '@vueuse/core';
 
-const scrollBox = ref(null);
-const scroll = useScroll(scrollBox);
+const el = ref<HTMLElement>(null);
+const data = ref([]);
+const total = ref(0);
+
+useInfiniteScroll(el, loadData, { distance: 10 });
+
+loadData();
+
+async function loadData() {
+  if (data.value.length !== 0 && data.value.length >= total.value) return;
+  const res = await fetch(
+    `https://dummyjson.com/products/?limit=10&skip=${data.value.length}`
+  );
+  const d = await res.json();
+  total.value = d.total;
+  data.value = data.value.concat(d.products);
+}
 </script>
 
 <template>
-  <div class="scroll-box" ref="scrollBox">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-    facilisisMorbi facilisisMorbiddddddddddddddddddddddddddddddddddddddd
-    facilisisMorbi Morbi facilisisMorbi facilisisMorbi facilisisfacilisisMorbi
-    facilisis lorem felis mauris, vel dapibus nulla molestie laoreet. Nunc
-    porttitor metus faucibus ante pellentesque, ac maximus augue cursus. Fusce
-    hendrerit, eros dictum accumsan hendrerit.
+  <div ref="el" class="el">
+    <div v-for="item in data" class="card">
+      <img :src="item.thumbnail" alt="" />
+      <h3>{{ item.title }}</h3>
+      <p>{{ item.description }}</p>
+    </div>
   </div>
-  <pre>  {{ scroll }}</pre>
 </template>
 
 <style>
-.scroll-box {
-  background: gray;
-  height: 150px;
-  width: 200px;
-  overflow: scroll;
-  position: relative;
-  padding: 10px;
+html,
+body,
+#app {
+  height: 100%;
+}
+.el {
+  height: 100%;
+  overflow-y: scroll;
+}
+.card {
+  margin: 30px 10px;
+  box-shadow: 0 0 3px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  padding: 20px;
+}
+img {
+  border-radius: 10px;
+  display: block;
+  width: 100%;
 }
 </style>
