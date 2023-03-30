e5bca7cf8600827579c0c73839a2b69d4360ab47
Develing1991 <completed0728@gmail.com>
2023-03-30 10:34:22 +0900

    useBattery

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <script setup>
 import { ref, watch } from 'vue';
-import { useMagicKeys, whenever } from '@vueuse/core';
+import { useBattery } from '@vueuse/core';
 
-const { a_b_c, shift_a, meta_a, ctrl_a, current } = useMagicKeys();
-whenever(a_b_c, (pressed) => console.log(pressed));
-// const { a, b, c } = useMagicKeys();
-// whenever(a, (pressed) => console.log(pressed));
-// whenever(b, (pressed) => console.log(pressed));
-// whenever(c, (pressed) => console.log(pressed));
+const battery = useBattery();
 </script>
 
 <template>
-  {{ current }}
+  {{ battery }}
 </template>
 
 <style></style>
