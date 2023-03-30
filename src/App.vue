2b0dacc9fe5d387a1e4599598c34bb10c8e3505c
Develing1991 <completed0728@gmail.com>
2023-03-30 11:09:51 +0900

    delete me

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,10 @@
 <script setup>
 import { ref, reactive, watch, onMounted } from 'vue';
-import { usePointer } from '@vueuse/core';
-
-const pointer = reactive(usePointer());
-const canvas = ref(null);
-watch(pointer, () => {
-  if (pointer.pressure === 0) return;
-  const ctx = canvas.value.getContext('2d');
-  ctx.fillStyle = 'red';
-  ctx.fillRect(pointer.x - 20, pointer.y - 20, 10, 10);
-});
-onMounted(() => {
-  canvas.value.height = document.body.clientHeight;
-  canvas.value.width = document.body.clientWidth;
-});
+import { onLongPress } from '@vueuse/core';
 </script>
 
 <template>
-  <canvas ref="canvas"></canvas>
-  <!-- <pre>  {{ pointer }}</pre> -->
+  <button>Click and Hold</button>
 </template>
 
 <style></style>
