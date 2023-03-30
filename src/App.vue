bcb36eb9da263b5fddf782a404605d6b9ac93d7b
Develing1991 <completed0728@gmail.com>
2023-03-30 16:24:55 +0900

    useLocalStorage

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,8 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useRefHistory, useDebouncedRefHistory } from '@vueuse/core';
-const newTodo = ref('');
-const todos = ref([]);
-
-const { history, undo, redo } = useRefHistory(todos, {
-  deep: true,
-  capacity: 10, // 히스토리 용량 최대 10
-});
+import { useLocalStorage } from '@vueuse/core';
+const name = useLocalStorage('name', 'lee');
 </script>
 
 <template>
-  <input type="text" v-model="newTodo" placeholder="New Todo" />
-  <button
-    @click="
-      todos.unshift(newTodo);
-      newTodo = '';
-    "
-  >
-    Create Todo
-  </button>
-  <button @click="undo">Undo</button>
-  <button @click="redo">Redo</button>
-  <ul>
-    <li v-for="todo in todos" :key="todo">{{ todo }}</li>
-  </ul>
-  <br />
-  <pre>{{ history }}</pre>
+  <input type="text" v-model="name" />
 </template>
