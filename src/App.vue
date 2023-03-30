6f305a51112ea9c3f5b221ed20bf0ca59e6bffae
Develing1991 <completed0728@gmail.com>
2023-03-30 15:59:04 +0900

    useRefHistory

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,30 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { useTransition, TransitionPresets } from '@vueuse/core';
-const percent = ref(0);
-const disabled = ref(false);
-const color = ref('blue');
-const animatedPercent = useTransition(percent, {
-  transition: TransitionPresets.easeInOutCubic,
-  duration: 3000,
-  disabled,
-  onStarted() {},
-  onFinished() {
-    color.value = 'green';
-  },
+import { useRefHistory, useDebouncedRefHistory } from '@vueuse/core';
+const newTodo = ref('');
+const todos = ref([]);
+
+const { history, undo, redo } = useRefHistory(todos, {
+  deep: true,
+  capacity: 10, // 히스토리 용량 최대 10
 });
-const load = () => {
-  percent.value = 100;
-};
 </script>
 
 <template>
-  <div
-    class="loading"
-    :style="`width: ${animatedPercent}%; background: ${color}`"
-  ></div>
-  {{ Math.floor(animatedPercent) }} %
-  <button @click="load">Load</button>
-  <button @click="disabled = true">disabled</button>
+  <input type="text" v-model="newTodo" placeholder="New Todo" />
+  <button
+    @click="
+      todos.unshift(newTodo);
+      newTodo = '';
+    "
+  >
+    Create Todo
+  </button>
+  <button @click="undo">Undo</button>
+  <button @click="redo">Redo</button>
+  <ul>
+    <li v-for="todo in todos" :key="todo">{{ todo }}</li>
+  </ul>
+  <br />
+  <pre>{{ history }}</pre>
 </template>
-
-<style>
-.loading {
-  height: 10px;
-  /* background: "blue" */
-}
-</style>
